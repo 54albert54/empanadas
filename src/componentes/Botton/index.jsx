333f82830b77e6a17e04823b535a5f488735079c
54albert54 <126289455+54albert54@user.noreply.github.com>
2023-06-29 19:39:28 -0400

    verificando link desde el movil

--- src/componentes/Botton/index.jsx
+++ src/componentes/Botton/index.jsx
@@ -3,9 +3,13 @@ import "./style.css"
 export const BottonWS =({children,numWs,mensaje})=>{
 return(
 <div className="Botton">
-<a title="Click para chatear" href={`https://api.whatsapp.com/send?phone=${numWs}&text=${mensaje}`} target="blank" rel="noopener"><p className="Botton_p" >{children}</p> </a>
+
+<a title="Click para chatear" href={`whatsapp://send?text=${mensaje}&phone=${numWs}`} target="blank" rel="noopener"><p className="Botton_p" >{children}</p> </a>
 
     
    </div>
 )
 };
+
+// https://api.whatsapp.com/send?phone=${numWs}&text=${mensaje}
+// whatsapp://send?text=${mensaje}&phone=${numWs}
--- src/componentes/MyPedido/index.jsx
+++ src/componentes/MyPedido/index.jsx
@@ -9,7 +9,7 @@ export const MyPedido =()=>{
 
   let numWs = 9786011460
   
-  let jugosP ="jugos: "
+  let jugosP ="Jugos de: "
   const calcularJugos =()=>{
     
 
@@ -21,7 +21,7 @@ export const MyPedido =()=>{
         
         return( 
           
-        <div className="Pedido_container" key={index}> <p>{jugos.sabor}</p>  <p>__</p> <p>{jugos.precios}</p></div>)})
+        <div className="Pedido_container" key={index}> <p>{jugos.sabor+"    "}</p>   <p>{jugos.precios+" $"}</p></div>)})
     )
   }
   
@@ -31,9 +31,9 @@ export const MyPedido =()=>{
   
 return(
   <>
-<div >
+<div>
   
-<h1 > jugos</h1>
+<h1> Jugos</h1>
 
 {
   calcularJugos()
@@ -43,10 +43,15 @@ return(
 </div>
 <p>total : {total}</p>
 {context.jugos.length>0?
-<div onClick={()=>{context.setJugos([])}}>
-<BottonWS numWs={numWs} mensaje={jugosP +" para un total: "+total+"dolares"} > finalizar pedidos</BottonWS>
+<div  onClick={()=>{context.setJugos([])}}>
+<BottonWS numWs={numWs} mensaje={jugosP +" para un total: "+total+"  dolares"} > finalizar pedidos</BottonWS>
 </div>:<div className="Botton_desabilitado"> <p>Has tu pedido primero</p></div>}
+<a  href="whatsapp://send?text=Tu mensaje&phone=+19786011460"> movil</a>
+<a className="whatsappLink mobile" href="whatsapp://send?text=esto lo cambie desde la app de github de mi cel&phone=+19786011460"> mossvil</a>
 
 </>
 )
 };
+
+{/* <a className="whatsappLink mobile" href="whatsapp://send?text=Tu mensaje&phone=+19786011460"> movil</a> <br></br> */}
+{/* <a className="whatsappLink desktop" href="http://web.whatsapp.com/send?text=Tu mensaje&phone=+19786011460">pc</a> */}

8465c590701239287da4d9b4543b044e9ed091fa
54albert54 <126289455+54albert54@user.noreply.github.com>
2023-07-01 21:27:18 -0400

    cambios menores

--- src/componentes/Acerca/index.jsx
+++ src/componentes/Acerca/index.jsx
@@ -10,28 +10,22 @@ const Reconfir =()=>{
 
 return(
 <>
-
-
 <div onClick={()=>context.sethasNumber(false)} className="cambiar_boton"> Cambiar numero </div>
-
 </>
 )
 };
 
-// hasNumber, sethasNumber
 export const Acerca =()=>{
   const context =useContext(AppContext)
 return(
 <div>
   <h3 className="">Puequeña intro</h3>
-  <p>Bueno aqui es donde va la descripcion y mucho blabla bla
+  <p>Bueno aqui es donde va la descripcion y mucho blabla bla donde hablas de todas las virtudesd e tu trabajo
   </p>
 <h2> Numero contacto: {context.numero}</h2>
  <p>{context.link==context.linkCel?"Enviaras mensaje desde el cel":"enviaras mensajes desde la web"}</p>
 <Boton funcion={context.changeLink}/>
-
 {!context.hasNumber?<AgregarNumero/>:<Reconfir/>}
-  
 </div>
 )
 };
